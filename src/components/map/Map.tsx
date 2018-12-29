c3b67abfb81432107e33990550a09e834b7a51a1
nesidbod <nesidbod@gmail.com>
2018-12-29 21:09:15 +0300

    added bounds for map updated icon sensors added grid

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,32 +1,222 @@
-import * as React from 'react'
-import '../../styles/map/Maps.css'
 import { Button, Fab } from '@material-ui/core'
+import * as React from 'react'
 import GoogleMapView from '../../sComponents/gMap/GoogleMapView'
-
+import '../../styles/map/Maps.css'
+// import map from '../../styles/img/map.jpg'
 interface ISettingsProps {
+  history: any
 }
 
 class Map extends React.Component<ISettingsProps, any> {
   constructor(props: ISettingsProps) {
     super(props)
-
     this.state = {
       openPopup: false,
-      center: { lat: 32.793587, lng: 35.052716 },
+      center: { lat: 32.79572308736829, lng: 35.05576364815124 },
+      sizeMap: 100,
+      lastX: 0,
+      newX: 0,
+      lastY: 0,
+      newY: 0,
+      mouseDown: false,
       activeCenter: { type: '', index: 0 },
       indications: [
-        { color: 'yellow', value: 3, id: 1 },
-        { color: 'orange', value: 1, id: 2 },
-        { color: 'red', value: 2, id: 3 }],
+        { color: 'yellow', value: 0, id: 1 },
+        { color: 'orange', value: 4, id: 2 },
+        { color: 'red', value: 1, id: 3 }],
+
       indicationsData: [
-        { lat: 32.79586030509149, lng: 35.05520652773009, type: 'green', id: 9 },
-        { lat: 32.79456159975777, lng: 35.05681585313903, type: 'yellow', time: '11:30', mess: 'yellow error', open: false, id: 1 },
-        { lat: 32.798024771830185, lng: 35.0554425621234, type: 'yellow', time: '11:20', mess: 'yellow error', open: false, id: 2 },
-        { lat: 32.793966353474204, lng: 35.061150302907095, type: 'red', time: '13:30', mess: 'red error', open: false, id: 3 },
-        { lat: 32.79890858059838, lng: 35.05906890871154, type: 'red', time: '11:00', mess: 'red error', open: false, id: 4 },
-        { lat: 32.79892661742052, lng: 35.054391136189565, type: 'orange', time: '14:30', mess: 'orange error', open: false, id: 5 },
-        { lat: 32.796670561366994, lng: 35.051813672834896, type: 'yellow', time: '11:30', mess: 'yellow error', open: false, id: 6 },
-        { lat: 32.79154782686155, lng: 35.0557404268327, type: 'green', id: 7 }
+        { lat: 32.78800244000398, lng:  35.05891807758121, type: 'green', id: 0 },
+        { lat:32.79929643743554, lng: 35.0545709406465, type: 'orange', time: '14:30', mess: 'orange error', open: false, id: 1 },
+        { lat: 32.79817207921318, lng: 35.06037943093315, type: 'orange', time: '11:30', mess: 'orange error', open: false, id: 2 },
+        { lat: 32.7934131259387, lng: 35.069190619830124, type: 'red', time: '13:30', mess: 'red error', open: false, id: 3 },
+        { lat:32.79537630438263, lng: 35.05928508965508, type: 'green', id: 4 },
+        { lat: 32.789838607019284, lng:35.05716078011528, type: 'orange', time: '12:30', mess: 'orange error', open: false, id: 5 },
+        { lat: 32.797216114483895, lng: 35.06151668755547, type: 'orange', time: '10:30', mess: 'orange error', open: false, id: 6 },
+        { lat: 32.79483455650888, lng: 35.056410117685346, type: 'green', id: 7 },
+        { lat: 32.79586030509149, lng: 35.063855573816454, type: 'green', id: 8 },
+        { lat:  32.79402347855168, lng: 35.06241790978447, type: 'green', id: 9 },
+        { lat: 2.79446675401682, lng: 35.06022037517823, type: 'green', id: 10 },
+        { lat:32.79230936413287, lng: 35.05961828488694, type: 'green', id: 11 }, 
+        { lat: 32.791065227696734, lng: 35.05902759758965, type: 'green', id: 12 },
+        { lat: 32.798330086211436, lng: 35.05561264298581, type: 'green', id: 13 },
+        { lat: 32.79355449412625, lng: 35.05218260018364, type: 'green', id: 14 },
+        { lat: 32.79806385730691, lng: 35.05265466897026, type: 'green', id: 15 },
+        { lat: 32.795863316643505, lng:35.05519740311638, type: 'green', id: 16 }, 
+        { lat: 32.796855322000425, lng: 35.054023872278776, type: 'green', id: 17 },
+        { lat: 32.7973063002748, lng: 35.05695693223015, type: 'green', id: 18 },
+        { lat: 32.79628719551429, lng: 35.058072731180346, type: 'green', id: 19 },
+      ],
+      testData: [
+        {
+          data: '14/12/2018', indications: [
+            { type: 'green', id: 0 },
+            {  type: 'green', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'green', id: 4 },
+            { type: 'green', id: 5 },
+            {  type: 'green', id: 6 },
+            {  type: 'green', id: 7 },
+            { type: 'green', id: 8 },
+            {  type: 'green', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'green', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'green', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '15/12/2018', indications: [
+            {  type: 'green', id: 0 },
+            {  type: 'orange', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'green', id: 4 },
+            {  type: 'yellow', id: 5 },
+            {  type: 'green', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'green', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'green', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'yellow', id: 17 },
+            {  type: 'green', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '16/12/2018', indications: [
+            {  type: 'green', id: 0 },
+            {  type: 'green', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'yellow', id: 4 },
+            {  type: 'green', id: 5 },
+            {  type: 'green', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'yellow', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'yellow', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'yellow', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '18/12/2018', indications: [
+            {  type: 'orange', id: 0 },
+            {  type: 'green', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'green', id: 4 },
+            {  type: 'green', id: 5 },
+            {  type: 'orange', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'green', id: 9 },
+            {  type: 'orange', id: 10 },
+            {  type: 'orange', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'orange', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'yellow', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '19/12/2018', indications: [
+            {  type: 'green', id: 0 },
+            {  type: 'yellow', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'yellow', id: 4 },
+            {  type: 'green', id: 5 },
+            {  type: 'yellow', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'red', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'red', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'yellow', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'yellow', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '20/12/2018', indications: [
+            {  type: 'green', id: 0 },
+            {  type: 'green', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'red', id: 4 },
+            {  type: 'green', id: 5 },
+            {  type: 'green', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'green', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'red', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'green', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+        {
+          data: '21/12/2018', indications: [
+            {  type: 'red', id: 0 },
+            {  type: 'green', id: 1 },
+            {  type: 'green', id: 2 },
+            {  type: 'green', id: 3 },
+            {  type: 'green', id: 4 },
+            {  type: 'green', id: 5 },
+            {  type: 'green', id: 6 },
+            {  type: 'green', id: 7 },
+            {  type: 'green', id: 8 },
+            {  type: 'green', id: 9 },
+            {  type: 'green', id: 10 },
+            {  type: 'green', id: 11 },
+            {  type: 'green', id: 12 },
+            {  type: 'green', id: 13 },
+            {  type: 'green', id: 14 },
+            {  type: 'green', id: 15 },
+            {  type: 'green', id: 16 },
+            {  type: 'green', id: 17 },
+            {  type: 'yellow', id: 18 },
+            {  type: 'green', id: 19 },
+          ]
+        },
+
       ]
     }
   }
@@ -39,7 +229,8 @@ class Map extends React.Component<ISettingsProps, any> {
         <div className="map">
           <div className="map-header">
             {indications.map((el: any, index: number) => {
-              return <Fab className={`map-header-item ${el.color}`} key={index} onClick={() => this.setCenter(el.color)}>
+              return <Fab className={`map-header-item ${el.color}`} key={index} 
+              onClick={() => this.setCenter(el.color)}>
                 {el.value}
               </Fab>
             })}
@@ -58,6 +249,7 @@ class Map extends React.Component<ISettingsProps, any> {
               showDetails={(id: string, value: boolean) => this.showDetails(id, value)}
               setHideCenter={this.setHideCenter}
             />
+
           </div>
           <div className="map-share">
             <Button variant="contained" >
@@ -87,7 +279,7 @@ class Map extends React.Component<ISettingsProps, any> {
   private setCenter = (type: string) => {
     const { activeCenter, indicationsData } = this.state
     const count = activeCenter.index === indicationsData.lenght ? 0 : activeCenter.index
-    let saveData: any = {}
+    const saveData: any = {center: this.state.center, activeCenter: {index: 0}}
     let trig = false
 
     if (activeCenter.type === type) {
--- src/sComponents/gMap/GoogleMapView.tsx
+++ src/sComponents/gMap/GoogleMapView.tsx
@@ -1,23 +1,44 @@
 import * as React from 'react'
-import { GoogleMap, withGoogleMap, OverlayView } from "react-google-maps"
+import { GoogleMap, OverlayView, Polyline, withGoogleMap } from "react-google-maps"
 import green from '../../styles/img/green white.png'
 import orange from '../../styles/img/orange.png'
 import red from '../../styles/img/red white.png'
 
+const strictBounds = new google.maps.LatLngBounds(
 
-// var strictBounds = new google.maps.LatLngBounds(
-//   new google.maps.LatLng(32.79681422306506, 35.045930479711615),
-//   new google.maps.LatLng(32.79248517796432, 35.07519874448212)
-// );
+  new google.maps.LatLng(32.782746714452266, 35.049999581842826),
+  new google.maps.LatLng(32.80092927862214, 35.071972238092826),
+);
 
+const grid = [
+  { coord: [{ lat: 32.797123796856795, lng: 35.050316608854246 }, { lat: 32.786435630590866, lng: 35.063192603828156 }] },
+  { coord: [{ lat: 32.798956684094144, lng: 35.05046598641411 }, { lat: 32.787339054114504, lng: 35.06451701522633 }] },
+  { coord: [{ lat: 32.79972324539429, lng: 35.051624700708544 }, { lat: 32.788233151454484, lng: 35.0657009336187 }] },
+  { coord: [{ lat: 32.80038158100007, lng: 35.052375719232714 }, { lat: 32.788846474978605, lng: 35.06638757912651 }] },
+  { coord: [{ lat: 32.80102921090588, lng: 35.05335772307751 }, { lat: 32.78952105826433, lng: 35.067049688775114 }] },
+  { coord: [{ lat: 32.80156555468347, lng: 35.05430073745083 }, { lat: 32.79031701833976, lng: 35.067771010020124 }] },
+
+  { coord: [{ lat: 32.80159903811219, lng: 35.05842678277031 }, { lat: 32.79469087520042, lng: 35.050101205988085 }] },
+  { coord: [{ lat: 32.79332284136129, lng: 35.05086379264594 }, { lat: 32.800663986841236, lng: 35.059511234509955 }] },
+  { coord: [{ lat: 32.7923062486163, lng: 35.0518568683608 }, { lat: 32.79950318451458, lng: 35.06061159858541 }] },
+  { coord: [{ lat: 32.79124561166362, lng: 35.05322329728142 }, { lat: 32.79858080645209, lng: 35.06195105400468 }] },
+  { coord: [{ lat: 32.790199379562885, lng: 35.05445711342827 }, { lat: 32.79782488378683, lng: 35.063094620115294 }] },
+  { coord: [{ lat: 32.78783629280292, lng: 35.05672089783684 }, { lat: 32.796049186403984, lng: 35.06635737918407 }] },
+  { coord: [{ lat: 32.789459794273476, lng: 35.05495063988701 }, { lat: 32.79728826312394, lng: 35.064134523554 }] },
+  { coord: [{ lat: 32.796364756581234, lng: 35.04928366896047 }, { lat: 32.80272323509375, lng: 35.05670328887891 }] },
+
+]
+
+let mapSet: any = {}
 interface IMapProps {
   position: any
   isOpen: boolean
+  indicationsData: any[]
   onOpen(value: boolean): any
   setHideCenter(value: any): any
-  indicationsData: any[]
   showDetails(id: string, value: boolean): any
 }
+
 
 const GoogleMapView = withGoogleMap((props: IMapProps) =>
   <GoogleMap
@@ -31,35 +52,36 @@ const GoogleMapView = withGoogleMap((props: IMapProps) =>
       rotateControl: false,
       fullscreenControl: false,
     }}
-    onClick={(event) => {
-      console.log(event.latLng.lat(), event.latLng.lng())
-    }}
-    // ref={map => { 
-    //   if (map) {
-    //   if (strictBounds.contains(map.getCenter())) return;
 
-    //   // We're out of bounds - Move the map back within the bounds
-    
-    //   var c = map.getCenter(),
-    //       x = c.lng(),
-    //       y = c.lat(),
-    //       maxX = strictBounds.getNorthEast().lng(),
-    //       maxY = strictBounds.getNorthEast().lat(),
-    //       minX = strictBounds.getSouthWest().lng(),
-    //       minY = strictBounds.getSouthWest().lat();
-    
-    //   if (x < minX) x = minX;
-    //   if (x > maxX) x = maxX;
-    //   if (y < minY) y = minY;
-    //   if (y > maxY) y = maxY;
-    
-    //   props.setHideCenter(new google.maps.LatLng(y, x));
-    //   }
-    // }}
+    onClick={(el) => console.log(el.latLng.lat(), el.latLng.lng())}
+    onDrag={() => {
+      if (!strictBounds.contains(mapSet.getCenter())) {
+        mapSet.panToBounds(strictBounds, 20)
+      }
+    }}
+    ref={map => {
+      if (map) {
+        mapSet = map;
+      }
+    }}
     mapTypeId={'satellite'}
     defaultCenter={props.position}
     center={props.position}
   >
+    {grid.map((el: any, index: number) => {
+      return <Polyline key={index}
+        options={{
+          path: el.coord,
+          strokeColor: '#0da20d',
+          strokeOpacity: 1,
+          strokeWeight: 2,
+          icons: [{
+            offset: '0',
+            repeat: '10px'
+          }],
+        }}
+      />
+    })}
 
     {props.indicationsData.map((el: any, index: number) => {
       return <OverlayView
@@ -72,9 +94,10 @@ const GoogleMapView = withGoogleMap((props: IMapProps) =>
             el.type === 'orange' ? orange :
               el.type === 'red' ? red : ''
           } />
-          <div className={`map-dot-time`} style={{ color: '#d81717' }}>{el.time && el.time}</div>
+          {el.time ? <div className={`map-dot-time`} style={{ color: '#d81717' }}>{el.time}</div> : ''}
           {el.mess ? el.open && <div className="map-dot-mess">
-            <span className="map-dot-mess-close" onClick={(event: any) => (event.stopPropagation(), props.showDetails(el.id, false))}> x</span>
+            <span className="map-dot-mess-close"
+              onClick={(event: any) => (event.stopPropagation(), props.showDetails(el.id, false))}> x</span>
             {el.mess}
           </div> : ''}
         </div>
